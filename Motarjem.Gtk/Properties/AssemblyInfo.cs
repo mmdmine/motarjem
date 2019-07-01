d4e02f70c0b61c01c746c11ee6a594dd59c1d9be
Mohammad Amin <asm262015@gmail.com>
2019-07-01 11:11:53 +0430

    release update

--- Motarjem.Gtk/Properties/AssemblyInfo.cs
+++ Motarjem.Gtk/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Motarjem.Gtk")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Motarjem GTK")]
+[assembly: AssemblyDescription("Motarjem GTK")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Motarjem.Gtk")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyCompany("Mohammad Amin Mollazadeh")]
+[assembly: AssemblyProduct("Motarjem GTK")]
+[assembly: AssemblyCopyright("Copyright ©  2019 Mohammad Amin Mollazadeh")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.2.0.0")]
+[assembly: AssemblyFileVersion("0.2.0.0")]
