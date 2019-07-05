6a4069ca8f6c1966a7f95a0686483ffdc6b5d535
Mohammad Amin <asm262015@gmail.com>
2019-07-05 13:01:11 +0430

    possible nested adjectives

--- Motarjem.Core/Phrases/AdjectiveNoun.cs
+++ Motarjem.Core/Phrases/AdjectiveNoun.cs
@@ -7,7 +7,7 @@ namespace Motarjem.Core.Phrases
     internal class AdjectiveNoun : NounPhrase
     {
         public Word Adjective;
-        public Nominal Right;
+        public NounPhrase Right;
 
         protected override void DisplayEnglish(IDisplay display)
         {
@@ -22,13 +22,25 @@ namespace Motarjem.Core.Phrases
             if (Right != null)
             {
                 Right.Display(display, Language.Persian);
-                var noun = Right.Right as Noun;
-                if (noun == null) return;
-                if ("هوا".Contains(noun.Word.Persian.Last()))
+                var nominal = Right as Nominal;
+                if (nominal != null)
                 {
-                    // TODO: should remove space after Noun
-                    display.Print("ی");
-                    display.PrintSpace();
+                    var noun = nominal.Right as Noun;
+                    if (noun != null &&
+                        "هوا".Contains(noun.Word.Persian.Last())) 
+                    {
+                        // TODO: should remove space after Noun
+                        display.Print("ی");
+                        display.PrintSpace();
+                    }
+                }
+                else
+                {
+                    var adj = Right as AdjectiveNoun;
+                    if (adj != null)
+                    {
+                        adj.Display(display, Language.Persian);
+                    }
                 }
             }
 
@@ -39,9 +51,13 @@ namespace Motarjem.Core.Phrases
         internal static AdjectiveNoun ParseEnglish(Queue<Word[]> words)
         {
             var adj = new AdjectiveNoun {Adjective = words.Dequeue().First(a => a.Pos == PartsOfSpeech.Adjective)};
-            if (words.Any()
-                && words.Peek().Any(a => a.IsNoun))
-                adj.Right = Nominal.ParseEnglish(words);
+            if (words.Any())
+            {
+                if (words.Peek().Any(a => a.Pos == PartsOfSpeech.Adjective))
+                    adj.Right = ParseEnglish(words);
+                else if (words.Peek().Any(a => a.IsNoun))
+                    adj.Right = Nominal.ParseEnglish(words);
+            }
             return adj;
         }
     }
